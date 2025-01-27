8653afeb5394c11d48ab084f427830244ab1ee1e
Vetrichelvan <pythonhubdev@gmail.com>
2025-01-27 21:37:56 +0530

    feat(terminals): added config for ghostty and wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,60 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- Keys
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+
+	-- Maximize wezterm window
+	{ key = "n", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
+
+	{
+		key = "q",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "'",
+		mods = "CTRL",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
+}
+
+-- Make task numbers clickable
+-- the first matched regex group is captured in $1.
+table.insert(config.hyperlink_rules, {
+	regex = [[\b[tt](\d+)\b]],
+	format = "https://example.com/tasks/?t=$1",
+})
+
+-- Make username/project paths clickable. This implies paths like the following are for GitHub.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+table.insert(config.hyperlink_rules, {
+	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://www.github.com/$1/$3",
+})
+
+-- And finally, return the configuration to wezterm
+return config
